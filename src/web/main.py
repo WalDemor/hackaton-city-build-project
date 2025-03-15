56c682226bc3461e7d227df345042d79b40f4e3e
inProcess... <59766838+lnterestingToTry@users.noreply.github.com>
2025-03-15 11:52:36 +0200

    init

---
+++ src/web/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify, url_for, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def start():
+    return redirect(url_for('home'))
+
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    return render_template("base.html")
+
+@app.route("/news", methods=['GET', 'POST'])
+def news():
+    return render_template("base.html")
+
+@app.route("/about", methods=['GET', 'POST'])
+def about():
+    return render_template("base.html")
+
+@app.route("/projects", methods=['GET', 'POST'])
+def projects():
+    return render_template("base.html")
+
+@app.route("/contact", methods=['GET', 'POST'])
+def contact():
+    return render_template("base.html")
